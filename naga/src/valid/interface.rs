8aa00b0ef4b2903b542741213f3ad186c4c32186
Vin Howe <24789592+vinhowe@users.noreply.github.com>
2025-10-07 17:51:33 -0600

    Remove check that fleetwood did

--- naga/src/valid/interface.rs
+++ naga/src/valid/interface.rs
@@ -192,13 +192,13 @@ impl VaryingContext<'_> {
                     return Err(VaryingError::UnsupportedCapability(required));
                 }
 
-                if matches!(
-                    built_in,
-                    crate::BuiltIn::SubgroupId | crate::BuiltIn::SubgroupInvocationId
-                ) && ep.workgroup_size[1..].iter().any(|&s| s > 1)
-                {
-                    return Err(VaryingError::InvalidMultiDimensionalSubgroupBuiltIn);
-                }
+                // if matches!(
+                //     built_in,
+                //     crate::BuiltIn::SubgroupId | crate::BuiltIn::SubgroupInvocationId
+                // ) && ep.workgroup_size[1..].iter().any(|&s| s > 1)
+                // {
+                //     return Err(VaryingError::InvalidMultiDimensionalSubgroupBuiltIn);
+                // }
 
                 let (visible, type_good) = match built_in {
                     Bi::BaseInstance
